5f3b3efb7619b9ce06806db503522407732a9b86
NehaVerma009 <nehuverma009@gmail.com>
2023-05-22 14:49:59 +0530

    Flight Prices

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-const express = require express
-const bodyparser = require bodyparser
+
+const axios = require('axios');
+// const date = Date.now
+
+async function getFlightPrices(origin, destination, Date) {
+  try {
+    const response = await axios.get('https://www.makemytrip.com/flights/', {
+      params: {
+        origin,
+        destination,
+        Date
+      }
+    });
+
+  
+    const flights = [response.data]
+    const formattedPrices= []
+    flights.forEach(flight=>{
+        formattedPrices[flight.airlines] = `₹${flight.price}`
+    })
+ // })
+ console.log(formattedPrices)
+} 
+  catch (error) {
+    console.error('Error retrieving flight prices:', error.message);
+  }
+}
+
+
+getFlightPrices('Delhi', 'Jaipur','20May2023');
+
+
+  
+
+ 
+
+
